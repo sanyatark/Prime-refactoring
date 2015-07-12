5526f0bf308821e1f35762c80d1e8e02a163c631
Tark <sanyatark@gmail.com>
2015-07-12 18:28:31 +0800

    Update script.rb

--- script.rb
+++ script.rb
@@ -1,13 +1,16 @@
-require 'prime'
+require 'prime'   # This is a module. We'll cover these soon!
 
 def first_n_primes(n)
-  # Check for correct input!
-  "n must be an integer" unless n.is_a? Integer
-  "n must be greater than 0" if n <= 0
 
-  # The Ruby 1.9 Prime class makes the array automatically!
-  prime = Prime.instance
-  prime.first n
+  puts "n must be an integer." unless n.is_a? Integer
+
+  puts "n must be greater than 0." if n <= 0
+  
+  prime_array ||= []
+  
+  prime = Prime.new
+  n.times {prime_array <<(prime.next)}
+  prime_array
 end
 
 first_n_primes(10)
